487397f30f2d4974e86984b3159f476800824359
MagicLong <longke467@gmail.com>
2015-01-17 01:51:35 +0800

    fix bugs & add message post request

--- app/scripts/controllers/overview.js
+++ app/scripts/controllers/overview.js
@@ -47,18 +47,30 @@ angular.module('dianApp')
                     .put(config.api_url + '/registration/registration/' + header_reg.id + '/', {
                         "status": "turn"
                     })
-                    .success(function(data, status, headers, config){
+                    .success(function(data, status, headers, conf){
                         header_reg = data;
                         table_type.queue_registrations.shift();
                         table_type.current_registration = header_reg;
+
+                        // 发短信通知当前queue中排在第二位置的客户
+                        if (table_type.queue_registrations[1]){
+                            $http
+                                .post(config.api_url + '/registration/msg-task/', {
+                                    "registration": table_type.queue_registrations[1].id
+                                })
+                                .success(function(data, status, headers, config){
+
+                                })
+                                .error(function(data, status, headers, config){
+
+                                });
+                        }
                     })
                     .error(function(data, status, headers, config){
                     });
             }else{
                 table_type.current_registration = null;
             }
-
-            // TODO 发送短信
         };
 
         $scope.status_change = function(table, status) {
