04b277652e3b8629510fe3c4c08dea84b8f23b18
jiwang <jiwang@alibaba-inc.com>
2015-06-06 17:39:18 +0800

    resturant menu management skeleton

--- app/scripts/controllers/console.menu.js
+++ app/scripts/controllers/console.menu.js
@@ -18,6 +18,17 @@ angular.module('dianApp')
             })
     }])
 
-    .controller('MenuCtrl', ['$scope', function() {
+    .controller('MenuCtrl', ['$scope', function($scope) {
+      $scope.copy_of_table_types = $scope.menu_types = [{
+        name: 'a dinner',
+        list: [
+          {
+            img: 'http://rp.dk26.com:3080/20150523/images/%E8%8F%9C%E5%8D%95%E7%AE%A1%E7%90%86/u94.png',
+            name: '123',
+            price: 123,
+            unit: '盘'
+          }
+        ]
+      }];
       
     }]);
