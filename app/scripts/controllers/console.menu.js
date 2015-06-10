fd7005745c0a969416b9f453dc36ab52d39d9802
jiwang <jiwang@alibaba-inc.com>
2015-06-10 23:28:03 +0800

    refine code format & add removal modal for product

--- app/scripts/controllers/console.menu.js
+++ app/scripts/controllers/console.menu.js
@@ -15,51 +15,66 @@ angular.module('dianApp')
                 url: '/menu',
                 templateUrl: 'views/console_menu.html',
                 controller: 'MenuCtrl'
-            })
+            });
     }])
 
-    .controller('MenuCtrl', ['$scope', '$http', function($scope, $http) {
-      $http.get(config.api_url + '/menu/list-menu/').then(function(res) {
-        console.log('list-menu:');
-        console.log(res.data);  
-        $scope.menus = res.data;
-        return $scope.menu = $scope.menus[0];
-      }).then(function(menu) {
-        console.log('menu selected:');
-        console.log(arguments);
-        return $http.get(config.api_url + '/menu/list-category-by-menu/' + menu.id + '/')
-      }).then(function(res) {
-        console.log('menu_catogaries:');
-        console.log(res.data);
-        $scope.menu_catogaries = res.data;
-        
-      });
-      $scope.get_category_products = function(category_id) {
-        $http.get(config.api_url + '/menu/list-product-by-category/' + category_id + '/').then(function(res) {
-          console.log('category_products:');
-          console.log(res.data);
-          $scope.category_products = res.data;
-        }, function(res) {
-          
-        })
-      };
-      $scope.menu_new = function(restaurant_id, menu_id) {
-        console.log('menu_new:');
-        //$http.post(api_url + '/menu/create-menu/', {})  
-      };
-      /*
-      $scope.copy_of_table_types = $scope.menu_types = [{
-        name: 'a dinner',
-        id: '1'
-        list: [
-          {
-            img: 'http://rp.dk26.com:3080/20150523/images/%E8%8F%9C%E5%8D%95%E7%AE%A1%E7%90%86/u94.png',
-            name: '123',
-            price: 123,
-            unit: '盘'
-          }
-        ]
-      }];
-      */
-     
+    .controller('MenuCtrl', ['$scope', '$http', '$modal',  function($scope, $http, $modal) {
+        $http.get(config.api_url + '/menu/list-menu/').then(function(res) {
+            console.log('list-menu:');
+            console.log(res.data);
+            $scope.menus = res.data;
+            return $scope.menu = $scope.menus[0];
+        }).then(function(menu) {
+            console.log('menu selected:');
+            console.log(arguments);
+            return $http.get(config.api_url + '/menu/list-category-by-menu/' + menu.id + '/')
+        }).then(function(res) {
+            console.log('menu_catogaries:');
+            console.log(res.data);
+            $scope.menu_catogaries = res.data;
+
+        });
+        $scope.get_category_products = function(category_id) {
+            $http.get(config.api_url + '/menu/list-product-by-category/' + category_id + '/').then(function(res) {
+                console.log('category_products:');
+                console.log(res.data);
+                $scope.category_products = res.data;
+            }, function(res) {
+
+            })
+        };
+        $scope.menu_new = function(restaurant_id, menu_id) {
+            console.log('menu_new:');
+            //$http.post(api_url + '/menu/create-menu/', {})
+        };
+
+
+        $scope.del_product = function(product_id){
+            var product_del_modalInstance = $modal.open({
+                templateUrl: 'product_del.html',
+                controller: 'ModalDelCtrl'
+            });
+
+            product_del_modalInstance.result.then(function (data) {
+                return $http
+                    .get(config.api_url + '/menu/delete-product/' + product_id + '/')
+            }).then(function(res) {
+              console.log('del_product ok');
+
+            }, function(res) {
+              console.error('del_product error');
+            });
+        };
+
+    }])
+
+    .controller('ModalDelCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance){
+
+        $scope.confirm = function(){
+            $modalInstance.close();
+        };
+
+        $scope.cancel = function(){
+            $modalInstance.dismiss();
+        };
     }]);
