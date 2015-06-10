fcf9a80ebefcb9dbe1f87d96354ded887b0f367b
jiwang <jiwang@alibaba-inc.com>
2015-06-10 21:51:10 +0800

    display menu types work

--- app/scripts/controllers/console.menu.js
+++ app/scripts/controllers/console.menu.js
@@ -18,9 +18,23 @@ angular.module('dianApp')
             })
     }])
 
-    .controller('MenuCtrl', ['$scope', function($scope) {
+    .controller('MenuCtrl', ['$scope', '$http', function($scope, $http) {
+      $http.get(config.api_url + '/menu/list-menu/').then(function(res) {
+        console.log('list-menu:');
+        console.log(res.data);  
+        $scope.copy_of_table_types = $scope.menu_types = res.data;
+      }, function(res) {
+      });
+      $scope.menu_products = function(restaurant_id, menu_id) {
+        console.log('menu_products:');
+        console.log(arguments);  
+
+
+      };
+      /*
       $scope.copy_of_table_types = $scope.menu_types = [{
         name: 'a dinner',
+        id: '1'
         list: [
           {
             img: 'http://rp.dk26.com:3080/20150523/images/%E8%8F%9C%E5%8D%95%E7%AE%A1%E7%90%86/u94.png',
@@ -30,5 +44,6 @@ angular.module('dianApp')
           }
         ]
       }];
+      */
       
     }]);
