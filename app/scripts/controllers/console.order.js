9af7c29fc1a0f702d1b4fe2cafa8f2198fad7dbd
jiwang <jiwang@alibaba-inc.com>
2015-06-16 21:12:54 +0800

    style and controller skeleton for dish up

---
+++ app/scripts/controllers/console.order.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dianApp.controller: OrderCtrl
+ * @description
+ * # OrderCtrl
+ * Controller of the dianApp
+ */
+
+angular.module('dianApp')
+
+    .config(['$stateProvider', function ($stateProvider) {
+        $stateProvider
+            .state('console.order', {
+                controller: 'OrderCtrl',
+                url: '/order',
+                templateUrl: 'views/console_order.html'
+            });
+    }])
+
+    .controller('OrderCtrl', ['$scope', '$http', function($scope, $http){
+    }]);
+
